8aec848f98cd94c6c85097b1c4051cacd3879c51
Lee Reeves <lee.reeves@gmail.com>
2022-07-06 07:53:51 -0700

    Add SAC Hopper benchmark, with LR annealing

---
+++ benchmarks/sac_hopper.py
@@ -0,0 +1,25 @@
+from torch_agents.agents.sac import ContinuousSAC
+from torch_agents import environments
+from torch_agents import schedule
+
+# Soft Actor Critic benchmark for Open AI Gym environment Hopper-V4
+# With hyperparameters from the SAC paper
+if __name__=="__main__":
+
+    env = environments.GymEnv(
+            name="Hopper-v4", 
+            render_mode=None
+            )
+
+    hp = ContinuousSAC.Hyperparams()
+    hp.max_actions=1000000
+    hp.warmup_actions = 10000
+
+    hp.actor_lr = schedule.Linear(hp.max_actions, 3e-4, 0).asfloat()
+    hp.critic_lr = schedule.Linear(hp.max_actions, 3e-4, 0).asfloat()
+
+    hp.reward_scale = 5
+    hp.temperature = 1 # schedule.Linear(hp.max_actions, 1, 0).asfloat()
+
+    agent = ContinuousSAC(env, hp)
+    agent.train()
