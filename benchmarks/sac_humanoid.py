fafeb1be2be935ea81be1d1009eba71f0bb05478
Lee Reeves <lee.reeves@gmail.com>
2022-07-25 01:59:44 -0700

    Verify and explain sac_humanoid benchmark

--- benchmarks/sac_humanoid.py
+++ benchmarks/sac_humanoid.py
@@ -4,8 +4,31 @@ from torch_agents import schedule
 
 from pprint import pprint
 
-# Soft Actor Critic benchmark for Open AI Gym environment Humanoid-V4
-# With hyperparameters from the SAC paper
+"""
+This benchmark replicates Soft Actor Critic performance 
+on Open AI Gym Humanoid environment as reported in:
+
+    Haarnoja, Tuomas, et al. "Soft actor-critic: Off-policy maximum entropy 
+    deep reinforcement learning with a stochastic actor." International 
+    conference on machine learning. PMLR, 2018.
+
+That paper reported results on Humanoid-V1, but this benchmark uses Humanoid-V4 
+because V4 uses current Mujoco libary bindings. This may cause some
+differences in performance.
+
+The reported hyperparameters are:
+    2 hidden layers with 256 units each
+    (all) learning rates = 3e-4
+    discount (gamma) = 0.99
+    replay buffer size = 1e6
+    target update rate (tau) = 0.005
+    target update interval = 1
+    gradient steps = 1
+    reward scale = 20
+
+The reported average returns are slightly over 4000 (4 points per step)
+after 1 million steps, and slightly over 6000 after 10 million steps.
+"""
 if __name__=="__main__":
 
     env = environments.GymEnv(
